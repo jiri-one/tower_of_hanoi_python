0c84f8703a8aad0c5151660eade42f178a7379ce
jiri.one <nemec@jiri.one>
2023-02-15 09:33:35 +0100

    small changes

--- towers.py
+++ towers.py
@@ -20,58 +20,62 @@ def move_disk(t_from: int, t_to: int):
 state after move {move_counter}
 ---------------------------"""))
 
-# print initial state of towers
-print("Tower 1:", towers[0])
-print("Tower 2:", towers[1])
-print("Tower 3:", towers[2])
-print(dedent(f"""---------------------------
+
+def main():
+    # print initial state of towers
+    print("Tower 1:", towers[0])
+    print("Tower 2:", towers[1])
+    print("Tower 3:", towers[2])
+    print(dedent(f"""---------------------------
 Initial towers state
 ---------------------------"""))
-
-# get tower with smallest disk on the top
-for index, tower in enumerate(towers):
-    if len(tower) > 0:
-        if tower[-1] == 1:
-            tower_with_one = index
-            break
-
-# cycle
-while True:
-    # move smallest disk (one)
-    if NUMBER_OD_DISKS % 2 == 0:
-        new_tower_with_one = (tower_with_one-1)%3
-    else:
-        new_tower_with_one = (tower_with_one+1)%3
-    move_disk(tower_with_one, new_tower_with_one)
-    tower_with_one = new_tower_with_one
-
-    # break cycle when disk one is on final position 
-    if towers[1] == INITIAL_TOWER:
-        break
     
-    # move with another disk (it is only one possible move)
-    two = [t for t in range(3) if t != new_tower_with_one] # towers without one
-    for ti in two: # ti like tower index
-        if len(towers[ti]) == 0:
-            tower_to = ti
-            two.remove(tower_to)
-            tower_from = two[0]
-            break
-    else:
-        if towers[two[0]][-1] > towers[two[1]][-1]:
-            tower_to = two[0]
-            tower_from = two[1]
-        else:
-            tower_to = two[1]
-            tower_from = two[0]
-    move_disk(tower_from, tower_to)
+    # get tower with smallest disk on the top
+    for index, tower in enumerate(towers):
+        if len(tower) > 0:
+            if tower[-1] == 1:
+                tower_with_one = index
+                break
 
-# print FINAL state of towers
-print("Tower 1:", towers[0])
-print("Tower 2:", towers[1])
-print("Tower 3:", towers[2])
-print(dedent(f"""---------------------------
+    # cycle
+    while True:
+        # move smallest disk (one)
+        if NUMBER_OD_DISKS % 2 == 0:
+            new_tower_with_one = (tower_with_one-1)%3
+        else:
+            new_tower_with_one = (tower_with_one+1)%3
+        move_disk(tower_with_one, new_tower_with_one)
+        tower_with_one = new_tower_with_one
+
+        # break cycle when disk one is on final position 
+        if towers[1] == INITIAL_TOWER:
+            break
+        
+        # move with another disk (it is only one possible move)
+        two = [t for t in range(3) if t != new_tower_with_one] # towers without one
+        for ti in two: # ti like tower index
+            if len(towers[ti]) == 0:
+                tower_to = ti
+                two.remove(tower_to)
+                tower_from = two[0]
+                break
+        else:
+            if towers[two[0]][-1] > towers[two[1]][-1]:
+                tower_to = two[0]
+                tower_from = two[1]
+            else:
+                tower_to = two[1]
+                tower_from = two[0]
+        move_disk(tower_from, tower_to)
+
+    # print FINAL state of towers
+    print("Tower 1:", towers[0])
+    print("Tower 2:", towers[1])
+    print("Tower 3:", towers[2])
+    print(dedent("""---------------------------
 FINAL towers state
 ---------------------------"""))
 
+if __name__ == "__main__":
+    main()
 #sorry guys, I wanted to do a little bit of testing in pytest and light automated testing in Github CI, but it's Valentine's Day .... ;-)
